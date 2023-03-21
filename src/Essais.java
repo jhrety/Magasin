a0388ac063b34c06f7ddbdcef7bd1e77fe6c5bca
Jean-Hugues RETY <jh.rety@iut.univ-paris8.fr>
2023-03-21 17:46:49 +0100

    ajout classe Essai.java

---
+++ src/Essais.java
@@ -0,0 +1,9 @@
+public class Essais {
+
+    public static void main(String[] args) {
+        System.out.println("mon Projet");
+        Produit stylo = new Produit("stylo bille vert", 200);
+        System.out.println(stylo);
+    }
+
+}
