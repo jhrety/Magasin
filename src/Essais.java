6487a35746e0d42eeca98f35bde0e25a376c8cb5
Jean-Hugues RETY <jh.rety@iut.univ-paris8.fr>
2023-03-21 20:01:27 +0100

    Ajout en début de Essais

--- src/Essais.java
+++ src/Essais.java
@@ -1,8 +1,8 @@
 public class Essais {
 
     public static void main(String[] args) {
-        System.out.println("mon Projet");
         Produit stylo = new Produit("stylo bille vert", 200);
+        System.out.println(stylo.getPrixHT());
         System.out.println(stylo);
     }
 
