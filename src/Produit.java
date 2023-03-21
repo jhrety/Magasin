5c6a742b86e203716e3e9af2f222e5a7ddb46e21
Jean-Hugues RETY <jh.rety@iut.univ-paris8.fr>
2023-03-21 17:25:25 +0100

    ajout classe Produit

---
+++ src/Produit.java
@@ -0,0 +1,34 @@
+public class Produit {
+
+    private String libellé;
+    private int prixHT;
+
+    public Produit(String libellé, int prixHT) {
+        this.libellé = libellé;
+        this.prixHT = prixHT;
+    }
+
+    public String getLibellé() {
+        return libellé;
+    }
+
+    public void setLibellé(String libellé) {
+        this.libellé = libellé;
+    }
+
+    public int getPrixHT() {
+        return prixHT;
+    }
+
+    public void setPrixHT(int prixHT) {
+        this.prixHT = prixHT;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "libellé='" + libellé + '\'' +
+                ", prixHT=" + prixHT +
+                '}';
+    }
+}
