305f6a55842f86f5f1ebe386d8a638ec066521be
Jean-Hugues RETY <jh.rety@iut.univ-paris8.fr>
2023-03-22 13:54:52 +0100

    stylo rouge

--- src/Essais.java
+++ src/Essais.java
@@ -3,9 +3,9 @@ public class Essais {
     public static void main(String[] args) {
     	
     	//Juste quelques essais basiques
-        Produit stylo = new Produit("stylo bille vert", 200);
-        System.out.println(stylo.getPrixHT());
-        System.out.println(stylo);
+        Produit styloRouge = new Produit("stylo bille rouge", 200);
+        System.out.println(styloRouge.getPrixHT());
+        System.out.println(styloRouge);
         Produit gomme = new Produit("gomme", 150);
         System.out.println(gomme);
         gomme.setLibellé("gomme à encre");
