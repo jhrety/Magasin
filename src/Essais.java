bcc25a61adeca60f8c844e9ab7aa82fea9eaa032
Jean-Hugues RETY <jh.rety@iut.univ-paris8.fr>
2023-03-22 08:06:50 +0100

    commentaire dans Essais

--- src/Essais.java
+++ src/Essais.java
@@ -1,6 +1,8 @@
 public class Essais {
 
     public static void main(String[] args) {
+    	
+    	//Juste quelques essais basiques
         Produit stylo = new Produit("stylo bille vert", 200);
         System.out.println(stylo.getPrixHT());
         System.out.println(stylo);
