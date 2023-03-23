8d764ed3906238c61fb7fe68b61228049268ed9c
Jean-Hugues RETY <jh.rety@iut.univ-paris8.fr>
2023-03-23 09:35:49 +0100

    ajout dans Stock d'une méthode qui retourne la quantité d'un produit

--- src/Stock.java
+++ src/Stock.java
@@ -13,7 +13,14 @@ public class Stock {
 	}
 
 	//TODO retirer produit
-	
+
+	public int quantitéDispo(Produit produit) {
+		for (LigneDeStock ligne : this.produitsEnStock)
+			if (ligne.getProduit() == produit)
+				return ligne.getQuantité();
+		return 0;
+	}
+
 	@Override
 	public String toString() {
 		return "Stock [produitsEnStock=" + produitsEnStock + "]";
